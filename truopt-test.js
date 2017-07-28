f3facd78eaa6fde29c4894060a91e78adc95e559
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-28 15:37:18 +0800

    Updated test.

--- truopt-test.js
+++ truopt-test.js
@@ -3,9 +3,9 @@ const assert = require( "assert" );
 const truopt = require( "./truopt.js" );
 
 assert.deepEqual( truopt( [ "orange", "apple" ] ),
-	{ "orange": true, "apple": true }, "should be deeply equal" );
+	{ "orange": true, "apple": true }, "should be equal to { 'orange': true, 'apple': true }" );
 
 assert.deepEqual( truopt( { "orange": true, "apple": true } ),
-	{ "orange": true, "apple": true }, "should be deeply equal" );
+	{ "orange": true, "apple": true }, "should be equal to { 'orange': true, 'apple': true }" );
 
 console.log( "ok" );
